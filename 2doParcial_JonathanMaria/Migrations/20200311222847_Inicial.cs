06dcedbb9fee78d34d08ef7184a7c75d6e52da82
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-03-11 18:29:03 -0400

    Migracion inicial agregada

---
+++ 2doParcial_JonathanMaria/Migrations/20200311222847_Inicial.cs
@@ -0,0 +1,58 @@
+﻿ion using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace _2doParcial_JonathanMaria.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Llamadas",
+                columns: table => new
+                {
+                    LlamadaId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Llamadas", x => x.LlamadaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LlamadasDetalles",
+                columns: table => new
+                {
+                    LlamadaDetalleId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Problema = table.Column<string>(nullable: true),
+                    Solucion = table.Column<string>(nullable: true),
+                    LlamadasLlamadaId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LlamadasDetalles", x => x.LlamadaDetalleId);
+                    table.ForeignKey(
+                        name: "FK_LlamadasDetalles_Llamadas_LlamadasLlamadaId",
+                        column: x => x.LlamadasLlamadaId,
+                        principalTable: "Llamadas",
+                        principalColumn: "LlamadaId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LlamadasDetalles_LlamadasLlamadaId",
+                table: "LlamadasDetalles",
+                column: "LlamadasLlamadaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LlamadasDetalles");
+
+            migrationBuilder.DropTable(
+                name: "Llamadas");
+        }
+    }
+}
