2a65b10cec68b2d96f37c84a6fb735fe18576aa2
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-03-11 18:28:25 -0400

    Contexto terminado!

---
+++ 2doParcial_JonathanMaria/Data/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using _2doParcial_JonathanMaria.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _2doParcial_JonathanMaria.Data
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Llamadas> Llamadas { get; set; }
+        public DbSet<LlamadasDetalles> LlamadasDetalles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = Database/2doParcial.Db");
+        }
+    }
+}
